597234a471f878941c2b202b30d254a9a74d7b29
Chris Hsu <yaostyle@gmail.com>
2020-03-30 14:02:36 -0700

    Add comments to the script and includ a requirements.txt file

--- master.py
+++ master.py
@@ -8,24 +8,37 @@ from subprocess import call
 loop = asyncio.get_event_loop()
 
 async def run(): 
+    """ Program main entry """
+    # Declar success number of counts (n)
     n = 0
+
+    # Resursive loop until reach 5 success tries
     while n <= 5: 
         rc = call(['./slave.py'], shell=True)
+        # If exit code is 0, add to success counts
         if rc == 0:
             n=n+1
+        # If exit code is 2, rerun the slave script
         elif (rc == 2):
             rc = call(['./slave.py'], shell=True)
+
+    # Finally reached to 5 success counts, run exit function 
     exit_loop(0)
         
 def exit_loop(int):
+    """ Exit function """
     try:
+        # Stop the loop, send SIGINT signal 
+        # and try exit the program
         loop.stop()
         signal.SIGINT
         sys.exit(int)
     except:
+        # No implement for exception handling yet
         pass
     
 if __name__ == '__main__':
+    # If running from the main script, start async loop
     asyncio.ensure_future(run())
     loop.run_forever()
 
--- slave.py
+++ slave.py
@@ -6,20 +6,28 @@ import random
 api = 'https://postman-echo.com/delay/'
 
 def run():
-    random_num = random.randint(0,5)
+    """ Program main entry """
+    # Get random num from 1-5
+    random_num = random.randint(0,5) 
     url = api+'{}'.format(random_num)
+
+    # Start requests sessions
     with requests.Session() as session:
         try:
             with session.get(url) as response:
                 print(response.status_code)
+                # If status code is 200, exit with 0
                 if response.status_code == 200:
                     sys.exit(0)
                 else:
+                    # Else, exist with 1
                     sys.exit(1)
         except(requests.exceptions.ConnectionError):
+            # If there's connection exception, exit with 2
             sys.exit(2)
 
 if __name__ == '__main__':
+    # If running from the main script, strat run it
     run()
                 
             

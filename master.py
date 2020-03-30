0889c80ef32ec7121e83f349e7e5c415dde68ab4
Chris Hsu <yaostyle@gmail.com>
2020-03-30 12:49:41 -0700

    Fixed return codes(0,1,2) and restar calling slave if getting 2

--- master.py
+++ master.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 import os
 import asyncio
-import aiohttp
-import json
-import ssl
-import subprocess as sp
+import signal
+import sys
+from subprocess import call
   
 loop = asyncio.get_event_loop()
 
-async def main():  
-    child = sp.Popen('./slave.py', stdout=sp.PIPE).communicate()[0]
+async def main(): 
+    n = 0
+    while n <= 5: 
+        rc = call(['./slave.py'], shell=True)
+        if rc == 0:
+            n=n+1
+        elif (rc == 2):
+            rc = call(['./slave.py'], shell=True)
+    exit(0)
+        
+def exit(int):
+    try:
+        loop.stop()
+        signal.SIGINT
+        sys.exit(int)
+    except:
+        pass
     
-    print(child)
-
-# async def main():
-#     client = aiohttp.ClientSession(loop=loop)
-#     while True:    
-#         await get_json(client, 'https://postman-echo.com/delay/1')
-
-# async def get_json(client, url):
-#     default_ssl = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
-#     async with client.get(url,ssl=default_ssl) as response:
-#         assert response.status == 200
-#         print("200")
-#         return await response.read()
-
 if __name__ == '__main__':
     asyncio.ensure_future(main())
     loop.run_forever()
--- slave.py
+++ slave.py
@@ -7,11 +7,15 @@ def run():
     random_num = random.randint(0,5)
     url = 'https://postman-echo.com/delay/{}'.format(random_num)
     with requests.Session() as session:
-        with session.get(url) as response:
-            if response.status_code == 200:
-                sys.exit(0)
-            else:
-                sys.exit(2)
+        try:
+            with session.get(url) as response:
+                print(response.status_code)
+                if response.status_code == 200:
+                    sys.exit(0)
+                else:
+                    sys.exit(1)
+        except(requests.exceptions.ConnectionError):
+            sys.exit(2)
 
 if __name__ == '__main__':
     run()
